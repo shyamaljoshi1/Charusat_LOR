941a6aa832d133f380e60783d2c585c4dd4c2616
Shyamal Joshi <shyamaljoshi2003@gmail.com>
2022-12-27 23:24:51 +0530

    initial setup

---
+++ client/lor/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className='frame'>
+      <div className='center'>
+        <img className='logo'  alt='logo' />
+        <div className='login-text'>Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
