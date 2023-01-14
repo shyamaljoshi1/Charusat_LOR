cd4dd6813a6f0e678766a7c1c4c6fe1d1c429295
emdhruvin <dhruvintandel0000@gmail.com>
2023-01-14 23:53:40 +0530

    placed through cdpc from lor request

--- client/lor/src/pages/lor_request/lor_request.js
+++ client/lor/src/pages/lor_request/lor_request.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./lor_request.css";
 
 const LorRequest = () => {
+  const [cdpc, setCdpc] = useState(false);
+  const onchange = (e) => {
+    setCdpc(e.target.value);
+  };
   return (
     <>
       <div className="Lor__container">
@@ -36,6 +41,42 @@ const LorRequest = () => {
                   <option value={"2024"}>2024</option>
                   <option value={"2025"}>2024</option>
                 </select>
+              </div>
+            </div>
+            <div className="place-through-cdpc">
+              <div className="place-through-cdpc__heading">
+                Placed Through CDPC, Charusat
+              </div>
+              <div className="radio__container">
+                <div className="radio__container__btn">
+                  <div className="radio-one">
+                    <input
+                      type="radio"
+                      id="yes"
+                      name="cdpc"
+                      value="true"
+                      onChange={onchange}
+                    />
+                    <label>Yes</label>
+                  </div>
+                  <div className="radio-two">
+                    <input
+                      type="radio"
+                      id="no"
+                      name="cdpc"
+                      value="false"
+                      onChange={onchange}
+                    />
+                    <label>No</label>
+                  </div>
+                </div>
+                {cdpc === "true" ? (
+                  <div className="radio__container__input__container">
+                    <input placeholder="Company Name" />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
               </div>
             </div>
           </form>
