aa4be1c95930315592b51f972838ed10740fd284
Dhruvin Tandel <dhruvintandel0000@gmail.com>
2023-02-08 14:31:18 +0530

    add Result Details

--- client/lor/src/pages/lor_request/lor_request.js
+++ client/lor/src/pages/lor_request/lor_request.js
@@ -4,34 +4,53 @@ import "./lor_request.css";
 // import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import PersonalInfo from "./personalInfo";
 import PlacementInfo from "./placementInfo";
+import ResultDetails from "./resultDetails";
 
 const LorRequest = () => {
   // const [cdpc, setCdpc] = useState(false);
   // const onchange = (e) => {
   //   setCdpc(e.target.value);
   // };
-  const [personalInfo,setPersonalinfo]=useState({
-    studentId:"",
-    studentName:"",
-    emailId:"",
-    studentMobile:"",
-    parentMobile:"",
-    passoutDate:"",
+  const [personalInfo, setPersonalinfo] = useState({
+    studentId: "",
+    studentName: "",
+    emailId: "",
+    studentMobile: "",
+    parentMobile: "",
+    passoutDate: "",
+  })
+  const [resultDetails, setResultDetails] = useState({
+    firstSAtt: "",
+    secondSAtt: "",
+    thirdSAtt: "",
+    fifthSAtt: "",
+    sixthSAtt: "",
+    seventhSAtt: "",
+    eightthSAtt: "",
+    firstSCG: "",
+    secondSCG: "",
+    thirdSACG: "",
+    forthSCG: "",
+    fifthSCG: "",
+    sixthSCG: "",
+    seventhSCG: "",
+    eightthSCG: "",
   })
 
   const onChange = (e) => {
     setPersonalinfo({ ...personalInfo, [e.target.name]: e.target.value });
+    setResultDetails({ ...resultDetails, [e.target.name]: e.target.value });
   };
 
-  // console.log(personalInfo);
+  // console.log(resultDetails);
 
   return (
-      <div className="form__container">
-        <form action="POST">
-          <PersonalInfo onChange={onChange}/>
-          <PlacementInfo/>
-        </form>
-      </div>
+    <div className="form__container">
+      <form action="POST">
+        <PersonalInfo onChange={onChange} />
+        <PlacementInfo />
+      </form>
+    </div>
   );
 };
 
